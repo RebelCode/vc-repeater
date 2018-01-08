c8b85336710d1770fcfe2eeb202c5224acd026de
roman <litvinenko95@gmail.com>
2018-01-08 13:56:20 +0200

    add usage and developing documentation to the readme.md, setup webpack

--- readme.md
+++ readme.md
@@ -1,1 +1,37 @@
 # Repeater Component
+
+## Usage
+```js
+<repeater :items="items">
+    <div slot-scope="r">{{ r.item.title }}</div>
+</repeater>
+```
+Where `items` has type of `FunctionalCollection` from [std-lib](https://github.com/RebelCode/std-lib).
+
+This example will iterate through all items in `items` object and output each item using child `repeater` code (`<div slot-scope="r"...` in our case). Next options is available in item's code:
+
+> `{scopeName}` is string you've passed to `slot-scope` attribute inside item's template. In our example is `r`. In that template you'll be able to access to exposed methods and variables using that scope.
+
+`{scopeName}.remove(item)` - function for removing item from the collection. Usefull when you want to add controls to your items (like `x` button for removing).
+
+`{scopeName}.item` - item that we iterating on from our collection.
+
+## Developing
+Run `npm install` to install all dev dependencies.
+
+Here is available npm commands.
+
+Build library while developing
+```sh
+npm run dev
+```
+
+Build library for production
+```sh
+npm run production
+```
+
+Run e2e tests
+```sh
+npm run e2e
+```
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+
+let config = {
+    context: __dirname,
+    devtool: debug ? "inline-sourcemap" : false,
+    plugins: debug ? [] : [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    ],
+    module: {
+        rules: [
+            { test: /\.js$/, loader: "babel-loader" }
+        ]
+    }
+};
+
+let entries = {
+    'e2e': {
+        entry: "./tests/e2e/app.js",
+        output: {
+            path: __dirname + "/build/js",
+            filename: "app.min.js",
+            libraryTarget: 'umd'
+        },
+        resolve: {
+            alias: {
+                vue: 'vue/dist/vue.js'
+            }
+        }
+    },
+    'default': {
+        entry: "./src/index.js",
+        output: {
+            path: __dirname + "/build/js",
+            filename: "lib.min.js",
+            libraryTarget: 'umd'
+        }
+    }
+}
+
+let e2e = process.env.NODE_ENV === 'e2e';
+config = Object.assign(config, entries[e2e ? 'e2e' : 'default']);
+
+module.exports = config;
