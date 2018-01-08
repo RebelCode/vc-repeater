0445390f795e4907fafd101e2aaba0bdbde409e0
roman <litvinenko95@gmail.com>
2018-01-08 14:06:14 +0200

    improved code documentation, with since tags

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
+/**
+ * Component for rendering items from the collection,
+ * it iterate through all items and gives ability
+ * to think only about rendered item template.
+ *
+ * @since [*next-version*]
+ *
+ * @param {Vue} Vue Vue instance.
+ * @returns {Component}
+ *
+ * @constructor
+ */
 export function CfRepeater(Vue) {
     return Vue.extend({
         props: {
+            /**
+             * Collection for rendering items from.
+             *
+             * @since [*next-version*]
+             *
+             * @property {FunctionalCollection}
+             */
             collection: {
                 required: true
             },
+
+            /**
+             * Tag used for wrapping rendered result.
+             *
+             * @since [*next-version*]
+             *
+             * @property {string}
+             */
             wrap: {
                 type: String,
                 default: 'div'
@@ -11,10 +38,12 @@ export function CfRepeater(Vue) {
         },
 
         computed: {
-            /*
+            /**
              * Get items from *injected* collection. Reactive.
              *
-             * @returns {*}
+             * @since [*next-version*]
+             *
+             * @return {Array} Collection items as array
              */
             items () {
                 return Object.values(this.collection.getItems())
@@ -22,6 +51,13 @@ export function CfRepeater(Vue) {
         },
 
         methods: {
+            /**
+             * Remove item from the collection.
+             *
+             * @since [*next-version*]
+             *
+             * @param {Object} item Some item from the collection.
+             */
             removeItem (item) {
                 this.collection.removeItem(item)
             },
@@ -29,7 +65,9 @@ export function CfRepeater(Vue) {
             /**
              * Provide additional slot properties. Can be extended by child.
              *
-             * @return {{}}
+             * @since [*next-version*]
+             *
+             * @return {Object}
              */
             getRepeatedSlotProps () {
                 return {};
@@ -38,13 +76,23 @@ export function CfRepeater(Vue) {
             /**
              * Additional repeater root options. Can be extended by child.
              *
-             * @return {{}}
+             * @since [*next-version*]
+             *
+             * @return {Object}
              */
             getRepeaterRootOptions () {
                 return {};
             }
         },
 
+        /**
+         * Render component.
+         *
+         * @since [*next-version*]
+         *
+         * @param {Function} createElement Function for rendering.
+         * @returns {VNode}
+         */
         render (createElement) {
             return createElement(this.wrap, this.getRepeaterRootOptions(), this.items.map((item) => {
                 let slotProps = Object.assign({
